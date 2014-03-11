15e682b79ab9b92dd65cd4c03c6ba9afa3a378ff
Martin Skow Røed <msroed@gmail.com>
2014-03-11 20:34:52 +0100

    delete image when not used anymore

--- readlikeme/core/profiles/models.py
+++ readlikeme/core/profiles/models.py
@@ -6,3 +6,12 @@ class Reader(AbstractUser):
 	followers = models.ManyToManyField('self', related_name='following', symmetrical=False, blank=True)
 	bio = models.CharField(max_length=200, blank=True, default="")
 	avatar = models.ImageField(upload_to='avatars/', default='avatars/no-img.png')
+
+	def save(self, *args, **kwargs):
+        # delete old file when replacing by updating the file
+        try:
+            this = Reader.objects.get(id=self.id)
+            if this.avatar != self.avatar:
+                this.image.delete(save=False)
+        except: pass # when new photo then we do nothing, normal case          
+        super(Photo, self).save(*args, **kwargs)
