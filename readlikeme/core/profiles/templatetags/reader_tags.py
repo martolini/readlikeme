5b296d69f685e67efdfcf2e9f91c0a13783cb3dd
Martin Skow Røed <msroed@gmail.com>
2014-03-13 14:27:22 +0100

    added who to follow

--- readlikeme/core/profiles/templatetags/reader_tags.py
+++ readlikeme/core/profiles/templatetags/reader_tags.py
@@ -1,10 +1,13 @@
 from django import template
-
+from readlikeme.core.profiles.models import Reader
+from django.db.models import Q
 register = template.Library()
 
 @register.inclusion_tag('reader_dashboard.jade', takes_context = True)
 def show_reader_dashboard(context):
-	return {'user': context['user']}
+	reader = context['user']
+	readers = Reader.objects.exclude(pk=reader.pk).exclude(id__in=reader.following.all())
+	return {'user': context['user'], 'suggested_readers': readers}
 
 @register.inclusion_tag('list_readers.jade', takes_context = True)
 def list_readers(context, readers):
