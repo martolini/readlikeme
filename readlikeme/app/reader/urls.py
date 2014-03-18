69e2509a5b747553fe723d890429444fbda24f14
Martin Skow Røed <msroed@gmail.com>
2014-03-18 21:54:06 +0100

    last removal paypal ipn

--- readlikeme/app/reader/urls.py
+++ readlikeme/app/reader/urls.py
@@ -8,10 +8,6 @@ view_patterns = patterns('readlikeme.app.reader.views',
 	url(r'^$', 'frontpage', name='frontpage'),
 	url(r'^post_article/$', 'post_article', name='post_article'),
 	url(r'^search/$', 'search_view', name="search"),
-	url(r'^credits/purchase/$', 'purchase_credits', name='purchase_credits'),
-	url(r'^something/paypal/', include('paypal.standard.ipn.urls')),
-	url(r'^credits/return/$', 'purchase_return', name='purchase_return'),
-	url(r'^credits/cancel/$', 'purchase_cancel', name='purchase_cancel'),
 )
 
 urlpatterns = ajax_patterns + view_patterns
